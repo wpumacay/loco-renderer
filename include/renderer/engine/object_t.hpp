f1358b6a87cd3158090bd0b9aa8af8a7162311a6
wpumacay <wpumacay@gmail.com>
2024-10-28 13:30:16 -0500

    (Update): refactored object_t class

--- include/renderer/engine/object_t.hpp
+++ include/renderer/engine/object_t.hpp
@@ -8,20 +8,19 @@
 
 namespace renderer {
 
-class Scene;
-
 /// All valid object types in the engine
-enum class ObjectType : uint8_t {
-    BASE = 0,  /// Base object type, used as dummy
-    MESH = 1,  /// 3d Mesh object type
+enum class RENDERER_API eObjectType : uint8_t {
+    BASE = 0,
+    MESH = 1,
+    CAMERA = 2,
+    LIGHT = 3,
 };
 
 /// Returns a string representation of the given object type enum
-/// \param[in] type The type of the object
-auto ToString(ObjectType type) -> std::string;
+RENDERER_API auto ToString(eObjectType type) -> std::string;
 
 /// Base interface for all objects supported by the engine
-class Object3D {
+class RENDERER_API Object3D {
     DEFAULT_COPY_AND_MOVE_AND_ASSIGN(Object3D)
 
     DEFINE_SMART_POINTERS(Object3D)
@@ -30,112 +29,37 @@ class Object3D {
     /// Creates a default Object3D
     Object3D() = default;
 
-    /// Creates an Object3D with the given name
-    /// \param[in] name The name of this object
-    explicit Object3D(std::string name);
-
-    /// Creates an Object3D with the given name and with given pose in world
-    /// space
-    /// \param[in] name The name of this object
+    /// Creates an object with given pose in world space
     /// \param[in] init_pose The pose of this object in world space
-    Object3D(std::string name, Pose init_pose);
+    explicit Object3D(Pose init_pose);
 
-    /// Creates an Object3D with the given name, position and orientation in
-    /// world space
-    /// \param[in] name The name of this object
+    /// Creates an object with given position and orientation in world space
     /// \param[in] init_pos The position of this object in world space
-    /// \param[in] init_quat The orientation of this object in world space
-    Object3D(std::string name, Vec3 init_pos, Quat init_quat);
+    explicit Object3D(Vec3 init_pos);
 
     /// Deallocates the resources used by this object
     virtual ~Object3D() = default;
 
-    /// Callback used when this object is assigned to a scene
-    /// \param[in] scene_handle A non-owning reference to the scene
-    auto SetScene(std::weak_ptr<Scene> scene_handle) -> void;
-
-    /// Sets the name of this object
-    /// \param[in]  new_name The new name to use for this object
-    auto SetName(std::string new_name) -> void;
-
-    /// Sets the pose of this object in world space
-    /// \param[in] new_pose The new pose of this object in world space
-    virtual auto SetPose(Pose new_pose) -> void;
-
-    /// Sets the position of this object in world space
-    /// \param[in] new_position The new position of this object in world space
-    virtual auto SetPosition(Vec3 new_position) -> void;
-
-    /// Sets the orientation of this object in world space
-    /// \param[in] new_quat The new orientation of this object in world space
-    virtual auto SetOrientation(Quat new_quat) -> void;
-
-    /// Sets the local pose of this object with respect to its parent
-    /// \param[in] pose The local pose of this object respect to its parent
-    virtual auto SetLocalPose(Pose pose) -> void;
-
-    /// Sets the local position of this object with respect to its parent
-    /// \param[in] position The local position of this object to its parent
-    virtual auto SetLocalPosition(Vec3 position) -> void;
-
-    /// Sets the local orientation of this object with respect to its parent
-    /// \param[in] orientation The local orientation of this object
-    virtual auto SetLocalOrientation(Quat orientation) -> void;
-
     /// Returns the type of this object
-    [[nodiscard]] auto type() const -> ObjectType { return m_Type; }
-
-    /// Returns the pose in world space of this object
-    [[nodiscard]] auto pose() const -> Pose { return m_WorldPose; }
-
-    /// Returns the position in world space of this object
-    [[nodiscard]] auto position() const -> Vec3 { return m_WorldPose.position; }
-
-    /// Returns the orientation in world space of this object
-    [[nodiscard]] auto orientation() const -> Quat {
-        return m_WorldPose.orientation;
-    }
-
-    /// Returns the local pose of this object with respect to its parent
-    [[nodiscard]] auto local_pose() const -> Pose { return m_LocalPose; }
-
-    /// Returns the local position of this object with respect to its parent
-    [[nodiscard]] auto local_position() const -> Vec3 {
-        return m_LocalPose.position;
-    }
-
-    /// Returns the local orientation of this object with respect to its parent
-    [[nodiscard]] auto local_orientation() const -> Quat {
-        return m_LocalPose.orientation;
-    }
-
-    /// Returns the name of this object
-    [[nodiscard]] auto name() -> std::string { return m_Name; }
+    RENDERER_NODISCARD auto type() const -> eObjectType { return m_Type; }
 
     /// Returns a string representation of this object
-    [[nodiscard]] auto ToString() const -> std::string;
+    RENDERER_NODISCARD auto ToString() const -> std::string;
 
- protected:
-    /// The name of this object
-    std::string m_Name;
-
-    /// The type of this object
-    ObjectType m_Type = ObjectType::BASE;
-
-    /// Non-owning reference to the scene if we are part of one
-    std::weak_ptr<Scene> m_Scene;
-
-    /// The 3d pose of this object respect to world space
-    Pose m_WorldPose;
-
-    /// The 3d pose of this object respect to its parent (if applicable)
-    Pose m_LocalPose;
+ public:
+    /// The 3d pose of this object respect to world space. If object has a
+    /// parent object, then this pose is relative to that parent object
+    Pose pose{};
 
     /// A weak reference to the parent of this object
-    std::weak_ptr<Object3D> m_Parent;
+    std::weak_ptr<Object3D> parent;
 
-    /// A container to hold shared ownership of any children
-    std::vector<Object3D::ptr> m_Children;
+    /// A container for the children of this object
+    std::vector<Object3D::ptr> children;
+
+ protected:
+    /// The type of this object
+    eObjectType m_Type{eObjectType::BASE};
 };
 
 }  // namespace renderer
--- source/renderer/engine/object_t.cpp
+++ source/renderer/engine/object_t.cpp
@@ -1,85 +1,36 @@
-#include <renderer/engine/object_t.hpp>
-#include <renderer/engine/scene_t.hpp>
+#include <string>
 
 #include <spdlog/fmt/bundled/format.h>
 
-#include <utility>
-#include <utils/logging.hpp>
+#include <renderer/engine/object_t.hpp>
 
 namespace renderer {
 
-auto ToString(ObjectType type) -> std::string {
+auto ToString(eObjectType type) -> std::string {
     switch (type) {
-        case ObjectType::BASE:
+        case eObjectType::BASE:
             return "Base";
-        case ObjectType::MESH:
+        case eObjectType::MESH:
             return "Mesh";
         default:
             return "Base";
     }
 }
 
-Object3D::Object3D(std::string name) : m_Name(std::move(name)) {}
+Object3D::Object3D(Pose init_pose) : pose(init_pose) {}
 
-Object3D::Object3D(std::string name, Pose init_pose)
-    : m_Name(std::move(name)), m_WorldPose(init_pose) {}
-
-Object3D::Object3D(std::string name, Vec3 init_pos, Quat init_quat)
-    : m_Name(std::move(name)), m_WorldPose(Pose(init_pos, init_quat)) {}
-
-auto Object3D::SetScene(std::weak_ptr<Scene> scene_handle) -> void {
-    m_Scene = std::move(scene_handle);
-}
-
-auto Object3D::SetName(std::string new_name) -> void {
-    if (!m_Scene.expired()) {
-        LOG_CORE_WARN(
-            "Object3D::SetName >>> Be careful that {0} already belongs to a "
-            "scene, where it might be stored by its name as key",
-            new_name);
-    }
-    m_Name = std::move(new_name);
-}
-
-auto Object3D::SetPose(Pose new_pose) -> void { m_WorldPose = new_pose; }
-
-auto Object3D::SetPosition(Vec3 new_position) -> void {
-    m_WorldPose.position = new_position;
-}
-
-auto Object3D::SetOrientation(Quat new_quat) -> void {
-    m_WorldPose.orientation = new_quat;
-}
-
-auto Object3D::SetLocalPose(Pose pose) -> void { m_LocalPose = pose; }
-
-auto Object3D::SetLocalPosition(Vec3 position) -> void {
-    m_LocalPose.position = position;
-}
-
-auto Object3D::SetLocalOrientation(Quat orientation) -> void {
-    m_LocalPose.orientation = orientation;
-}
+Object3D::Object3D(Vec3 init_pos) : pose(Pose(init_pos, Quat())) {}
 
 auto Object3D::ToString() const -> std::string {
-    std::string str_repr = "Object3D<\n";
-    str_repr += fmt::format("  name={0},\n", m_Name);
-    str_repr +=
-        fmt::format("  position={1},\n", m_WorldPose.position.toString());
-    str_repr +=
-        fmt::format("  orientation={2},\n", m_WorldPose.orientation.toString());
-    if (auto parent = m_Parent.lock()) {
-        str_repr += fmt::format("  parent={3},\n", parent->name());
-    }
-    if (!m_Children.empty()) {
-        str_repr += "  children=";
-        for (const auto& child : m_Children) {
-            str_repr += child->name() + ",";
-        }
-        str_repr += "\n";
-    }
-    str_repr += ">\n";
-    return str_repr;
+    return fmt::format(
+        "<Object3D\n"
+        "  position: {0}\n"
+        "  orientation: {1}\n"
+        "  parent: {2}\n"
+        "  children: {3}\n"
+        ">\n",
+        this->pose.position.toString(), this->pose.orientation.toString(),
+        (this->parent.expired() ? "Yes" : "None"), this->children.size());
 }
 
 }  // namespace renderer
