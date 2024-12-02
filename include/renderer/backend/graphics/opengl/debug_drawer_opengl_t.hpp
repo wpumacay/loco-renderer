0e22440b6382bdd9fc7685c25a23a130128ea9d6
wpumacay <wpumacay@gmail.com>
2024-12-02 12:44:38 -0500

    (feat): refactors impl of debug drawer

---
+++ include/renderer/backend/graphics/opengl/debug_drawer_opengl_t.hpp
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include <renderer/common.hpp>
+#include <renderer/engine/camera_t.hpp>
+#include <renderer/backend/graphics/opengl/program_opengl_t.hpp>
+#include <renderer/backend/graphics/opengl/vertex_array_opengl_t.hpp>
+
+namespace renderer {
+namespace opengl {
+
+/// Number of lines that can be drawn per batch
+static constexpr uint32_t LINES_BATCH_SIZE = 1024;
+/// Number of vertex positions stored per line
+static constexpr uint32_t POSITIONS_PER_LINE = 2;
+/// Number of vertex colors stored per line
+static constexpr uint32_t COLORS_PER_LINE = 2;
+/// Number of floats per vertex in each line
+static constexpr uint32_t FLOATS_PER_LINE =
+    3 * (POSITIONS_PER_LINE + COLORS_PER_LINE);
+/// The size of the VBO (in bytes) used to store both positions and colors
+static constexpr uint32_t LINES_VBO_SIZE =
+    sizeof(Vec3) * (POSITIONS_PER_LINE + COLORS_PER_LINE) * LINES_BATCH_SIZE;
+
+class RENDERER_API OpenGLDebugDrawer {
+    // cppcheck-suppress unknownMacro
+    NO_COPY_NO_MOVE_NO_ASSIGN(OpenGLDebugDrawer)
+
+    DEFINE_SMART_POINTERS(OpenGLDebugDrawer)
+ public:
+    struct Line {
+        Vec3 start;
+        Vec3 end;
+        Vec3 color;
+
+        explicit Line(Vec3 p_start, Vec3 p_end, Vec3 p_color)
+            : start(p_start), end(p_end), color(p_color) {}
+    };
+
+    OpenGLDebugDrawer();
+
+    ~OpenGLDebugDrawer() = default;
+
+    /// Requests a line to be drawn with the given properties
+    /// \param[in] start The start point of the line segment to be drawn
+    /// \param[in] end The end point of the line segment to be drawn
+    /// \param[in] color The color of the line to be drawn
+    auto DrawLine(Vec3 start, Vec3 end, Vec3 color) -> void;
+
+    /// Renders all primitives from the given viewpoint
+    /// \param[in] camera The camera used to render from
+    auto Render(const Camera& camera) -> void;
+
+    /// Returns a string representation of this debug drawer
+    RENDERER_NODISCARD auto ToString() const -> std::string;
+
+ protected:
+    /// Renders a single batch of lines into the current render target
+    /// \param[in] lines_number The number of lines to draw in this batch
+    /// \param[in] lines_data A pointer to the vertex data of this batch
+    auto _RenderBatchOfLines(size_t lines_number,
+                             const float32_t* lines_data) -> void;
+
+ protected:
+    /// Owned reference to the main shader used for debug drawing lines
+    OpenGLProgram::uptr m_LinesProgram{nullptr};
+
+    /// VAO used to handle all lines drawing
+    OpenGLVertexArray::uptr m_LinesVAO{nullptr};
+
+    /// Container for all lines being requested by the user
+    std::vector<Line> m_LinesContainer;
+};
+
+}  // namespace opengl
+}  // namespace renderer
---
+++ source/renderer/backend/graphics/opengl/debug_drawer_opengl_t.cpp
@@ -0,0 +1,166 @@
+#include <array>
+#include <memory>
+#include <string>
+#include <utility>
+
+#include <glad/gl.h>
+
+#include <spdlog/fmt/bundled/format.h>
+
+#include <renderer/backend/graphics/opengl/debug_drawer_opengl_t.hpp>
+
+namespace renderer {
+namespace opengl {
+
+constexpr const char* DD_VERT_SHADER_WIREFRAME_MODE_SRC = R"(
+#version 330 core
+
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 color;
+
+uniform mat4 u_proj_matrix;
+uniform mat4 u_view_matrix;
+
+out vec3 f_color;
+
+void main() {
+    gl_Position = u_proj_matrix * u_view_matrix * vec4(position, 1.0);
+    f_color = color;
+}
+)";
+
+constexpr const char* DD_FRAG_SHADER_WIREFRAME_MODE_SRC = R"(
+#version 330 core
+
+in vec3 f_color;
+out vec4 color;
+
+void main() {
+    color = vec4(f_color, 1.0);
+}
+)";
+
+OpenGLDebugDrawer::OpenGLDebugDrawer() {
+    m_LinesProgram = std::make_unique<OpenGLProgram>(
+        DD_VERT_SHADER_WIREFRAME_MODE_SRC, DD_FRAG_SHADER_WIREFRAME_MODE_SRC);
+    m_LinesProgram->Build();
+
+    OpenGLBufferLayout buff_layout = {
+        {"position", eElementType::FLOAT_3, false},
+        {"color", eElementType::FLOAT_3, false}};
+
+    auto lines_vbo = std::make_unique<OpenGLVertexBuffer>(
+        buff_layout, eBufferUsage::DYNAMIC, LINES_VBO_SIZE, nullptr);
+
+    m_LinesVAO = std::make_unique<OpenGLVertexArray>();
+    m_LinesVAO->AddVertexBuffer(std::move(lines_vbo));
+}
+
+auto OpenGLDebugDrawer::DrawLine(Vec3 start, Vec3 end, Vec3 color) -> void {
+    m_LinesContainer.emplace_back(start, end, color);
+}
+
+auto OpenGLDebugDrawer::Render(const Camera& camera) -> void {
+    // Render all lines first --------------------------------------------------
+    if (!m_LinesContainer.empty()) {
+        m_LinesProgram->Bind();
+        m_LinesProgram->SetMat4("u_proj_matrix",
+                                camera.ComputeProjectionMatrix());
+        m_LinesProgram->SetMat4("u_view_matrix", camera.ComputeViewMatrix());
+
+        const auto NUM_FULL_BATCHES =
+            m_LinesContainer.size() / LINES_BATCH_SIZE;
+        const auto NUM_REMAINING_LINES =
+            m_LinesContainer.size() % LINES_BATCH_SIZE;
+
+        std::array<float, LINES_BATCH_SIZE> lines_batch{};
+        for (uint32_t batch_idx = 0; batch_idx < NUM_FULL_BATCHES;
+             ++batch_idx) {
+            for (uint32_t line_idx = 0; line_idx < LINES_BATCH_SIZE;
+                 ++line_idx) {
+                const auto& line =
+                    m_LinesContainer[batch_idx * LINES_BATCH_SIZE + line_idx];
+                const auto VERTEX_START_IDX = FLOATS_PER_LINE * line_idx;
+
+                // First vertex has position and color attributes
+                lines_batch.at(VERTEX_START_IDX + 0) = line.start.x();
+                lines_batch.at(VERTEX_START_IDX + 1) = line.start.y();
+                lines_batch.at(VERTEX_START_IDX + 2) = line.start.z();
+
+                lines_batch.at(VERTEX_START_IDX + 3) = line.color.x();
+                lines_batch.at(VERTEX_START_IDX + 4) = line.color.y();
+                lines_batch.at(VERTEX_START_IDX + 5) = line.color.z();
+
+                // Second vertex has also position and color attributes
+                lines_batch.at(VERTEX_START_IDX + 6) = line.end.x();
+                lines_batch.at(VERTEX_START_IDX + 7) = line.end.y();
+                lines_batch.at(VERTEX_START_IDX + 8) = line.end.z();
+
+                lines_batch.at(VERTEX_START_IDX + 9) = line.color.x();
+                lines_batch.at(VERTEX_START_IDX + 10) = line.color.y();
+                lines_batch.at(VERTEX_START_IDX + 11) = line.color.z();
+            }
+            _RenderBatchOfLines(LINES_BATCH_SIZE, lines_batch.data());
+        }
+
+        if (NUM_REMAINING_LINES > 0) {
+            for (uint32_t line_idx = 0; line_idx < NUM_REMAINING_LINES;
+                 ++line_idx) {
+                const auto& line =
+                    m_LinesContainer[NUM_FULL_BATCHES * LINES_BATCH_SIZE +
+                                     line_idx];
+                const auto VERTEX_START_IDX = FLOATS_PER_LINE * line_idx;
+
+                // First vertex has position and color attributes
+                lines_batch.at(VERTEX_START_IDX + 0) = line.start.x();
+                lines_batch.at(VERTEX_START_IDX + 1) = line.start.y();
+                lines_batch.at(VERTEX_START_IDX + 2) = line.start.z();
+
+                lines_batch.at(VERTEX_START_IDX + 3) = line.color.x();
+                lines_batch.at(VERTEX_START_IDX + 4) = line.color.y();
+                lines_batch.at(VERTEX_START_IDX + 5) = line.color.z();
+
+                // Second vertex has also position and color attributes
+                lines_batch.at(VERTEX_START_IDX + 6) = line.end.x();
+                lines_batch.at(VERTEX_START_IDX + 7) = line.end.y();
+                lines_batch.at(VERTEX_START_IDX + 8) = line.end.z();
+
+                lines_batch.at(VERTEX_START_IDX + 9) = line.color.x();
+                lines_batch.at(VERTEX_START_IDX + 10) = line.color.y();
+                lines_batch.at(VERTEX_START_IDX + 11) = line.color.z();
+            }
+            _RenderBatchOfLines(NUM_REMAINING_LINES, lines_batch.data());
+        }
+
+        m_LinesProgram->Unbind();
+        m_LinesContainer.clear();
+    }
+    // -------------------------------------------------------------------------
+
+    // TODO(wilbert): render other debug primitives
+}
+
+auto OpenGLDebugDrawer::_RenderBatchOfLines(
+    size_t lines_number, const float32_t* lines_data) -> void {
+    auto& lines_vbo = m_LinesVAO->GetVertexBuffer(0);
+    const auto VBO_SUBSIZE = (POSITIONS_PER_LINE + COLORS_PER_LINE) *
+                             m_LinesContainer.size() * sizeof(Vec3);
+
+    m_LinesVAO->Bind();
+    lines_vbo.UpdateData(static_cast<uint32_t>(VBO_SUBSIZE), lines_data);
+    glDrawArrays(GL_LINES, 0, static_cast<GLsizei>(lines_number * 2));
+    m_LinesVAO->Unbind();
+}
+
+auto OpenGLDebugDrawer::ToString() const -> std::string {
+    return fmt::format(
+        "<OpenGLDebugDrawer\n"
+        "  linesBatchSize: {0}\n"
+        "  linesCount: {1}\n"
+        ">\n",
+        LINES_BATCH_SIZE, m_LinesContainer.size());
+}
+
+}  // namespace opengl
+
+}  // namespace renderer
